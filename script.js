4a5facdff3730868a7dd89ef4cc150064b81bb60
dodytrifa <dody.trifa@gmail.com>
2021-04-17 20:33:32 +0700

    finishing button

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const buttons = document.querySelectorAll('.ripple')
 
+buttons.forEach(button => {
+  button.addEventListener('click', function (e) {
+    const x = e.clientX
+    const y = e.clientY
+
+    const buttonTop = e.target.offsetTop
+    const buttonLeft = e.target.offsetLeft
+    
+    const xInner = x - buttonLeft
+    const yInner = y - buttonTop
+
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = yInner + 'px'
+    circle.style.left = xInner + 'px'
+
+    this.appendChild(circle)
+    // karena di append, harus diremove lagi
+
+    setTimeout(() =>circle.remove(),500)
+    
+  })
+})
